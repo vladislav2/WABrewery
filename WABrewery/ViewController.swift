1335c8549336db8034b162ba355a245805e2df85
vladislav2 <etter.id75@gmail.com>
2020-11-30 15:25:35 +0200

    Initial Commit

---
+++ WABrewery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WABrewery
+//
+//  Created by user on 30.11.2020.
+//  Copyright © 2020 Vlad Veretennikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
